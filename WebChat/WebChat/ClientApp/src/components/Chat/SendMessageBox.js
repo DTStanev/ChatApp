f24ef5013807db91dc16d956e3f8976c41c7ff41
Dimitar <dimityr.stanev86@gmail.com>
2019-01-11 17:19:09 +0200

    Added aditional functionality to SendMessageBox component

--- WebChat/WebChat/ClientApp/src/components/Chat/SendMessageBox.js
+++ WebChat/WebChat/ClientApp/src/components/Chat/SendMessageBox.js
@@ -5,8 +5,56 @@ import { faTelegramPlane } from '@fortawesome/free-brands-svg-icons'
 
 
 export default class SendMessageBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ''
+        }
+
+        this.handleMessageChange = this.handleMessageChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+        this.sendMessage = this.sendMessage.bind(this)
+    }
+
+    handleMessageChange(event) {
+       
+        let value = event.target.value;
+
+        this.setState({
+            message: value
+        })
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        console.log(this.state.message);
+        this.setState({
+            message: ''
+        });
+    }
+
+    sendMessage() {
+        let message = this.state.message;
+
+        if (message.length < 1) {
+            return;
+        }
+        document.getElementById("sendButton").click();        
+    }
 
     render() {
+
+        let element = document.getElementById('msg');
+
+        if (element) {
+            element.addEventListener('keydown', function (e) {
+                if (e.keyCode === 13) {
+                    this.sendMessage;
+                }
+            })
+        }
+
+
         return (
             <div className='panel-footer mt-3' id='message-holder'>
                 <form className='form-inline' onSubmit={this.onSubmit}>
@@ -16,9 +64,10 @@ export default class SendMessageBox extends Component {
                             onChange={this.handleMessageChange}
                             className='form-control bg-light'
                             id='msg'
+                            value={this.state.message}
                             placeholder='Your message'
                             autoComplete="off" />
-                        <button className='btn btn-primary chat-button input-group-addon'><FontAwesomeIcon icon={faTelegramPlane} /></button >
+                        <button id='sendButton' className='btn btn-primary chat-button input-group-addon'><FontAwesomeIcon icon={faTelegramPlane} /></button >
                     </div>
                 </form>
             </div>
